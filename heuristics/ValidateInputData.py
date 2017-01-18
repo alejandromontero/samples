606f3331b96bea2df2057cc9aa10df02921a253d
Alejandro Montero Rivero <alejandro.montero.rivero@gmail.com>
2017-01-18 20:42:20 +0100

    Update ValidateInputData.py

--- heuristics/ValidateInputData.py
+++ heuristics/ValidateInputData.py
@@ -1,22 +1,3 @@
-'''
-AMMM Lab Heuristics v1.0
-Instance file validator.
-Copyright 2016 Luis Velasco and Lluis Gifre.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 # Validate instance attributes read from a DAT file.
 # It validates the structure of the parameters read from the DAT file.
 # It does not validate that the instance is feasible or not.
